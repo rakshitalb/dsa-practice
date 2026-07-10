40a535f2b1503d70fc79026709d4dd30a22b7716
rakshita L B <rakshitallb@gmail.com>
2026-07-10 18:58:28 +0530

    Day 71: Fizz Buzz

---
+++ arrays/day71_fizz_buzz.py
@@ -0,0 +1,18 @@
+# Problem: Fizz Buzz
+# Day: 71
+
+n = int(input("enter the number: "))
+
+for i in range(1, n + 1):
+
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+
+    elif i % 3 == 0:
+        print("Fizz")
+
+    elif i % 5 == 0:
+        print("Buzz")
+
+    else:
+        print(i)
