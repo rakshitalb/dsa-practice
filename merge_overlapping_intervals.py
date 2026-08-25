e47447b648a9a58f6dde60f6c197f7059230f2b0
rakshita L B <rakshitallb@gmail.com>
2026-08-25 12:48:51 +0530

    Merge overlapping intervals

---
+++ merge_overlapping_intervals.py
@@ -0,0 +1,15 @@
+# Merge overlapping intervals
+
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+
+intervals.sort()
+
+ans = []
+
+for interval in intervals:
+    if not ans or ans[-1][1] < interval[0]:
+        ans.append(interval)
+    else:
+        ans[-1][1] = max(ans[-1][1], interval[1])
+
+print(ans)
