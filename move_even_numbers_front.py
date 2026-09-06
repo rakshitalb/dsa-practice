4700e6c0c44a9061117bb4292f5d5223cc118b08
rakshita L B <rakshitallb@gmail.com>
2026-09-06 12:16:52 +0530

    Move even numbers to the front using two pointers

---
+++ move_even_numbers_front.py
@@ -0,0 +1,16 @@
+n = list(map(int, input("enter :").split()))
+
+s = 0
+
+ans = []
+
+for f in range(len(n)):
+
+    if n[f] % 2 == 0:
+        n[s] = n[f]
+        s += 1
+
+    else:
+        ans.append(n[f])
+
+print(n[:s] + ans)
