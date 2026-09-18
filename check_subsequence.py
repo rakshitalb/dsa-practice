4ac5a77e5d8372cf2fd8ac569e7fd795767f385b
rakshita L B <rakshitallb@gmail.com>
2026-09-18 18:55:37 +0530

    Check whether one string is a subsequence of another

---
+++ check_subsequence.py
@@ -0,0 +1,15 @@
+s = input("Enter first string: ")
+t = input("Enter second string: ")
+
+i = 0
+j = 0
+
+while i < len(s) and j < len(t):
+    if s[i] == t[j]:
+        i += 1
+    j += 1
+
+if i == len(s):
+    print(True)
+else:
+    print(False)
