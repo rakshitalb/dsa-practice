78cf7862ec0c60a0b8807ad2b5873c5429c56eb7
rakshita L B <rakshitallb@gmail.com>
2026-09-26 08:54:32 +0530

    Day 108: Find First Occurrence in String using String Slicing

---
+++ find_first_occurrence_in_string_using_slicing.py
@@ -0,0 +1,10 @@
+class Solution(object):
+
+    def strStr(self, haystack, needle):
+
+        for i in range(len(haystack)):
+
+            if haystack[i:i + len(needle)] == needle:
+                return i
+
+        return -1
